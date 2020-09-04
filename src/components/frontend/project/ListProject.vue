7337938682bdd6527913bae5e2e0a0648d1a71c3
abhijeet-infobeans <abhijeet.dange@infobeans.com>
2020-09-04 12:32:41 +0530

    created project listing page, setup service worker with cache then network strategy

---
+++ src/components/frontend/project/ListProject.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>This is the Manage Project page.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ManageProject',
+  components: {
+  },
+  data: () => ({
+  }),
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
